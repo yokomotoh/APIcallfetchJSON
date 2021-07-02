0f805bfafb8b4c77a9126b4035bae67896694f09
yokomotoh <yokomotohama@hotmail.com>
2021-07-02 11:49:00 +0200

    Initial Commit

---
+++ APIcallfetchJSON/APIcallfetchJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIcallfetchJSONApp.swift
+//  APIcallfetchJSON
+//
+//  Created by vincent schmitt on 02/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct APIcallfetchJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
